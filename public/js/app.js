8b329cc03579c0abdc06d8250cb74b4efe991c07
Andre Laurio <45072321+Hyperstorm321@users.noreply.github.com>
2021-06-13 04:01:43 +0800

    Deploy to heroku
    Fix to forbidden error

--- public/js/app.js
+++ public/js/app.js
@@ -99054,8 +99054,8 @@ var opts = {};
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Andre\Desktop\Startup\school-mgmt\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Andre\Desktop\Startup\school-mgmt\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\Andre\Desktop\Outside projects\sama-sama\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Andre\Desktop\Outside projects\sama-sama\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
